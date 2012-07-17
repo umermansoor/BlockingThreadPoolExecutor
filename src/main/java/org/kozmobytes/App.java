2b7f8e91e25e76f0e0b57b09b5961d69c89c148a
Umer Mansoor <umermk3@gmail.com>
2012-07-16 21:17:54 -0600

    Implemented RejectedExecutionHandler

--- src/main/java/org/kozmobytes/App.java
+++ src/main/java/org/kozmobytes/App.java
@@ -1,13 +1,61 @@
 package org.kozmobytes;
 
+import java.util.concurrent.*;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private BlockingThreadPoolExecutor tpe;
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        App a = new App();
+        a.go();
+
+        for (int i=0; i < 10; i++)
+            try { a.newTask("task" + i); } catch (RejectedExecutionException rje) { System.out.println("task" + i + " Rejected");} 
+
+        
+        System.out.println("done here");
+    }
+                
+    public ThreadPoolExecutor getTpe()
+    {
+        return tpe;
+    }
+    
+    public void go()
+    {
+        tpe = new BlockingThreadPoolExecutor(3, 4, 2, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(2), new PriorityThreadFactory("thread"));
+        
+    }
+    
+    public void newTask(final String id) throws RejectedExecutionException
+    {
+        Runnable r = new Runnable()
+                {
+                    public void run()
+                    {
+                        int i = 0;
+                        System.err.println(id + " is running");
+                        
+                        while (i++ < 50)
+                        {
+                            try { Thread.sleep(100); } catch (InterruptedException e){ System.out.println("Intrrupted"); continue;}
+                        }
+                        
+                        //System.err.println(id + " is dead");
+                        
+                    }
+                    
+                };
+        
+        
+        tpe.execute(r);
+                
     }
 }
--- src/main/java/org/kozmobytes/BlockingThreadPoolExecutor.java
+++ src/main/java/org/kozmobytes/BlockingThreadPoolExecutor.java
@@ -1,46 +1,155 @@
 package org.kozmobytes;
 
 import java.util.concurrent.*;
+import org.slf4j.*;
 
 /**
- *
+ * Java's ThreadPoolExecutor is a great library, offering some very useful features. New tasks 
+ * (runnables) are submitted via ThreadPoolExecutor 's execute(...) method, which hands off 
+ * the task to one of its available workers or queue it.
+ * 
+ * However, if all the workers are busy, the pool is at its capacity and the queue is full, the
+ * task is rejected and execute(...) throws RejectedExecutionException. The responsibility then
+ * shifts back to the Caller thread which invoked execute(...) method to deal with the task
+ * rejection.
+ * 
+ * Often times, it is desirable to block (the execute(...) method) Caller if the task cannot be 
+ * accepted and to keep retrying task submission. There are several ways to achieve this, as
+ * explained in the article: 
+ * http://today.java.net/pub/a/today/2008/10/23/creating-a-notifying-blocking-thread-pool-executor.html
+ * 
+ * The BlockingThreadPoolExecutor class in this file implements a Blocking ThreadPoolExecutor as
+ * explained in the above article. We attach a customer RejectedExecutionHandler to ThreadPoolExecutor
+ * which keeps retrying task submission forever (until the thread-pool is alive). In addition, on 
+ * each subsequent rejection or acceptance, handler methods are called giving them a chance to take
+ * some application defined action.
+ * 
+ * Note: It is trivial to modify RejectedExecutionHandler to give up task submission after a specified
+ * number of retries or if a timeout occurs, throwing RejectedExecutionException.
+ * 
  * @author umermansoor
  */
 public class BlockingThreadPoolExecutor extends ThreadPoolExecutor
 {
+    protected  final Logger logger = LoggerFactory.getLogger(BlockingThreadPoolExecutor.class);
+    
+    /**
+     * RejectedExecutionHandler
+     */
+    private static final RejectedExecutionHandler rjeHandler = new RejectedExecutionHandler () {
+
+        public void rejectedExecution(Runnable r, ThreadPoolExecutor tpe) 
+        {
+            // Retry count for intrumentation and debugging
+            int retryCount = 0;
+            
+            // Try indefinitely to add the task to the queue
+            while (true)
+            {
+                retryCount++;
+                
+                if (tpe.isShutdown())  // If the executor is shutdown, reject the task and 
+                                       // throw RejectedExecutionException
+                {
+                    ((BlockingThreadPoolExecutor) tpe).taskRejectedGaveUp(r, retryCount);
+                    throw new RejectedExecutionException("ThreadPool has been shutdown");
+                }
+                
+                try
+                {
+                    if (tpe.getQueue().offer(r, 1, TimeUnit.SECONDS))
+                    {
+                        // Task got accepted! 
+                        ((BlockingThreadPoolExecutor) tpe).taskAccepted(r, retryCount);
+                        break;
+                    }
+                    else
+                        ((BlockingThreadPoolExecutor) tpe).taskRejectedRetrying(r, retryCount);
+                    
+                }
+                catch (InterruptedException e)
+                {
+                    throw new AssertionError(e);
+                }
+            } 
+        }
+        
+    };
+    
+   
+    /**
+     * Default constructor. Create a ThreadPool of a single thread with a very large queue.
+     */
     public BlockingThreadPoolExecutor()
     {
-        // Single core thread, normal priority
         this(1, 1, Integer.MAX_VALUE, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(), new PriorityThreadFactory("workerthread"));
     }
     
-    public BlockingThreadPoolExecutor(int corePoolSize, int maxPoolSize, int capacity, String baseThreadName, int priority)
+    /**
+     * Constructor
+     * @param corePoolSize - Number of threads of keep in the pool
+     * @param maxPoolSize - Maximum number of threads that can be spawned
+     * @param queueCapacity - Size of the task queue
+     * @param baseThreadName - Name assigned to each worker thread
+     * @param priority - Priority of worker threads
+     * @param daemon - If true, workers threads are created as daemons. 
+     * 
+     */
+    public BlockingThreadPoolExecutor(int corePoolSize, int maxPoolSize, int queueCapacity, String baseThreadName, int priority, boolean daemon)
     {
-        this(corePoolSize, maxPoolSize, Integer.MAX_VALUE, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(capacity), new PriorityThreadFactory(baseThreadName, priority));
+        this(corePoolSize, maxPoolSize, Integer.MAX_VALUE, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(queueCapacity), new PriorityThreadFactory(baseThreadName, priority, daemon));
     }
-
-  
-
+    
+    /**
+     * Constructor
+     * @param corePoolSize - Number of threads of keep in the pool
+     * @param maxPoolSize - Maximum number of threads that can be spawned
+     * @param keepAliveTime
+     * @param unit
+     * @param queue
+     * @param threadFactory 
+     */
     public BlockingThreadPoolExecutor(int corePoolSize, int maxPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> queue, ThreadFactory threadFactory)
     {
         super(corePoolSize, maxPoolSize, keepAliveTime, unit, queue, threadFactory);
         allowCoreThreadTimeOut(true);
          
-        /*
-         * Default behaviour of ThreadPoolExecutor's execute(...) function is to reject new tasks
-         * (runnables) in the work queue has reached it's capacity and no new threads can be created. 
-         * In this case, execute(...) throws RejectedExecutionException. However, in many cases, it 
-         * is desired to block on queue (keep retrying) until it has room. A custom RejectedExecutionHandler
-         * is provided below to that effect.
-         * [source: http://today.java.net/pub/a/today/2008/10/23/creating-a-notifying-blocking-thread-pool-executor.html]
-         */
-        
-        /**
-         * Alternatively, ThreadPoolExecutor also supports other actions. See the following link 
-         * for more info:
-         * [http://docs.oracle.com/javase/1.5.0/docs/api/java/util/concurrent/ThreadPoolExecutor.html]
-         */
-        //this.setRejectedExecutionHandler(blockingExecutionHandler);
+        // Attach a customer RejectedExecutionHandler defined above.
+        this.setRejectedExecutionHandler(rjeHandler);
     }
+    
+    /**
+     * Called when giving up on the task and rejecting it for good.
+     * @param r
+     * @param retryCount 
+     */
+    protected void taskRejectedGaveUp(Runnable r, int retryCount)
+    {
+        logger.debug("Gave Up: {}",retryCount );
+    }
+    
+    /**
+     * Called when the task that was rejected initially is rejected again.
+     * @param r - Task
+     * @param retryCount - number of total retries 
+     */
+    protected void taskRejectedRetrying(Runnable r, int retryCount)
+    {
+        logger.debug("Retrying: {}",retryCount );
+    }
+    
+    /**
+     * Called when the rejected task is finally accepted.
+     * @param r - Task
+     * @param retryCount - number of retries before acceptance
+     */
+    protected void taskAccepted(Runnable r, int retryCount)
+    {
+        logger.debug("Accepted: {}",retryCount );
+    }
+
+  
+
+    
     
 }
